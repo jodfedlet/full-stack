ae030579ae572d351bc250d83d12bdd3ddd0281d
Jod fedlet Pierre <fedletpierre15@gmail.com>
2019-05-31 20:08:52 -0300

    Add HTML

--- NodeJs/node.js
+++ NodeJs/node.js
@@ -1,7 +1,7 @@
 var http = require('http');
 
 var server = http.createServer(function(req, res) {
-  res.writeHead(200);
-  res.end('Salut tout le monde !');
+    res.writeHead(200, {"Content-Type": "text/html"});
+    res.end('<p>Voici un paragraphe <strong>HTML</strong> !</p>');
 });
 server.listen(8080);
