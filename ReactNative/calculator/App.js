1ae3c75a97caae4990b21d34d9e8efc1ae322eed
jodfedlet <prog33@ixcsoft.com.br>
2020-04-10 16:06:27 -0300

    End of Calc

--- ReactNative/calculator/App.js
+++ ReactNative/calculator/App.js
@@ -3,21 +3,63 @@ import { Platform, StyleSheet, Text, View } from 'react-native'
 import Button from './src/components/Button'
 import Display from './src/components/Display'
 
+const initialSate = {
+  displayValue: '0',
+  clearDisplay: false,
+  operation: null,
+  values: [0, 0],
+  current: 0
+}
 export default class App extends Component{
-  state = {
-    displayValue:'0'
-  }
+  state = { ...initialSate }
 
   addDigit = n => {
-    this.setState({ displayValue: n })
+    const clearDisplay = this.state.displayValue === '0'|| this.state.clearDisplay
+
+    if(n === '.' && !clearDisplay && this.state.displayValue.includes('.')){
+      return
+    }
+
+    const currentValue = clearDisplay ? '' : this.state.displayValue
+    const displayValue = currentValue + n
+
+    this.setState({ displayValue, clearDisplay: false })
+
+    if(n !== '.'){
+      const newValue = parseFloat(displayValue)
+      const values = [ ...this.state.values]
+      values[this.state.current] = newValue
+      this.setState({ values })
+    }
   }
 
   clearMemory = () => {
-    this.setState({displayValue: '0'})
+    this.setState({...initialSate})
   }
 
   setOperation = operation => {
+    if(this.state.current === 0){
+      this.setState({ operation, current: 1, clearDisplay: true})
+    }else{
+      const equals = operation === '='
+      const values = [ ...this.state.values]
 
+      try{
+        values[0] = eval(`${values[0]} ${this.state.operation} ${values[1]}`)
+      }catch(e){
+        values[0] = this.state.values[0]
+      }
+
+      values[1] = 0
+      this.setState({
+        displayValue: `${values[0]}`,
+        operation: equals ? null : operation,
+        current: equals ? 0 : 1,
+        clearDisplay: !equals,
+        values,
+
+      })
+    }
   }
 
   render(){
